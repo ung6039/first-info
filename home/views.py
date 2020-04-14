225cf7ffaa6c3e70c63daa3e01bbf65b80282130
unknown <ung6039Wnaver.com>
2020-04-14 14:26:25 +0900

    2020-04-14

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
 
 # Create your views here.
 
 def home_view(request):
     return render(request,"index.html", {})
+
+def homework(request):
+    return HttpResponse("<h1>인생은 B(brith) D(Death) 사이의 C(Chicken)이다.</h1>")
 
 
--- mocha/urls.py
+++ mocha/urls.py
@@ -7,7 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add an import:   from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from home.views import home_view
+from home.views import homework
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view),
+    path('quote/',homework),
 ]
 
